23a32ca377c2ffd4d1bd60da1c928e0337f98495
helyao <helyao@qq.com>
2017-06-27 14:53:01 +0800

    test

--- TrafficScrapy/TrafficScrapy/pipelines.py
+++ TrafficScrapy/TrafficScrapy/pipelines.py
@@ -25,19 +25,19 @@ class TrafficscrapyPipeline(object):
         print('host={host}, port={port}'.format(host=settings.MYSQL_HOST, port=settings.MYSQL_PORT))
 
     def process_item(self, item, spider):
-        try:
-            sql = 'INSERT INTO {table_name}(`id`, `name`, `startName`, `endName`, `time`, `roadGrade`, `avgspeed`, ' \
-                  '`sIndex`, `cIndex`, `bIndex`, `dir`, `rticLonlats`, `rticId`, `vkt`) ' \
-                  'VALUES("{id}", "{name}", "{startName}", "{endName}", "{time}", {roadGrade}, {avgspeed}, {sIndex}, {cIndex}, ' \
-                  '{bIndex}, "{dir}", "{rticLonlats}", "{rticId}", "{vkt}")' \
-                  ''.format(table_name=self.table_name,
-                            id=item['id'], name=item['name'], startName=item['startName'], endName=item['endName'],
-                            time=item['time'], roadGrade=item['roadGrade'], avgspeed=item['avgspeed'],
-                            sIndex=item['sIndex'], cIndex=item['cIndex'], bIndex=item['bIndex'],
-                            dir=item['dir'], rticLonlats=item['rticLonlats'], rticId=item['rticId'], vkt=item['vkt'])
-            print(sql)
+        # try:
+        sql = 'INSERT INTO {table_name}(`id`, `name`, `startName`, `endName`, `time`, `roadGrade`, `avgspeed`, ' \
+              '`sIndex`, `cIndex`, `bIndex`, `dir`, `rticLonlats`, `rticId`, `vkt`) ' \
+              'VALUES("{id}", "{name}", "{startName}", "{endName}", "{time}", {roadGrade}, {avgspeed}, {sIndex}, {cIndex}, ' \
+              '{bIndex}, "{dir}", "{rticLonlats}", "{rticId}", "{vkt}")' \
+              ''.format(table_name=self.table_name,
+                        id=item['id'], name=item['name'], startName=item['startName'], endName=item['endName'],
+                        time=item['time'], roadGrade=item['roadGrade'], avgspeed=item['avgspeed'],
+                        sIndex=item['sIndex'], cIndex=item['cIndex'], bIndex=item['bIndex'],
+                        dir=item['dir'], rticLonlats=item['rticLonlats'], rticId=item['rticId'], vkt=item['vkt'])
+        print(sql)
             # self.cursor.execute(sql)
             # self.connect.commit()
-        except:
-            pass
+        # except:
+        #     pass
         return item
