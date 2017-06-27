3f5872a1856d460647deea193eae183172836544
helyao <helyao@qq.com>
2017-06-27 13:55:30 +0800

    test

--- TrafficScrapy/TrafficScrapy/pipelines.py
+++ TrafficScrapy/TrafficScrapy/pipelines.py
@@ -7,31 +7,34 @@
 
 import pymysql
 from .basefunc import GetTimeStamp
+from TrafficScrapy import settings
 
 class TrafficscrapyPipeline(object):
 
-    def __init__(self, connect, table_info):
-        self.connect = connect
-        self.cursor = connect.cursor()
-        self.table_info = table_info
-
-    # Get Mysql Connection from settings.py
-    @classmethod
-    def from_settings(cls, settings):
-        connect = pymysql.connect(host=settings['MYSQL_HOST'], port=settings['MYSQL_PORT'],
-                                  user=settings['MYSQL_USER'], passwd=settings['MYSQL_PASSWD'],
-                                  db=settings)
-        table_info = settings['MYSQL_TABLE_INFO'] + '_' + GetTimeStamp()
-        return cls(connect, table_info)
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host=settings.MYSQL_HOST,
+            port=settings.MYSQL_PORT,
+            user=settings.MYSQL_USER,
+            passwd=settings.MYSQL_PASSWD,
+            db=settings.MYSQL_DATABASE
+        )
+        self.cursor = self.connect.cursor()
+        self.table_info = settings.MYSQL_TABLE_INFO + '_' + GetTimeStamp()
+        self.cursor.execute('create table {table} like {seed}'.format(table=self.table_info, seed=settings.MYSQL_TABLE_INFO))
 
     def process_item(self, item, spider):
-        sql = 'INSERT INTO tra_info(`id`, `name`, `startName`, `endName`, `time`, `roadGrade`, `avgspeed`, ' \
-              '`sIndex`, `cIndex`, `bIndex`, `dir`, `rticLonlats`, `rticId`, `vkt`) ' \
-              'VALUES("{id}", "{name}", "{startName}", "{endName}", "{time}", {roadGrade}, {avgspeed}, {sIndex}, {cIndex}, ' \
-              '{bIndex}, "{dir}", "{rticLonlats}", "{rticId}", "{vkt}")' \
-              ''.format(id=item['id'], name=item['name'], startName=item['startName'], endName=item['endName'],
-                        time=item['time'], roadGrade=item['roadGrade'], avgspeed=item['avgspeed'], sIndex=item['sIndex'],
-                        cIndex=item['cIndex'], bIndex=item['bIndex'], dir=item['dir'], rticLonlats=item['rticLonlats'],
-                        rticId=item['rticId'], vkt=item['vkt'])
-        self.cursor.execute(sql)
+        try:
+            sql = 'INSERT INTO tra_info(`id`, `name`, `startName`, `endName`, `time`, `roadGrade`, `avgspeed`, ' \
+                  '`sIndex`, `cIndex`, `bIndex`, `dir`, `rticLonlats`, `rticId`, `vkt`) ' \
+                  'VALUES("{id}", "{name}", "{startName}", "{endName}", "{time}", {roadGrade}, {avgspeed}, {sIndex}, {cIndex}, ' \
+                  '{bIndex}, "{dir}", "{rticLonlats}", "{rticId}", "{vkt}")' \
+                  ''.format(id=item['id'], name=item['name'], startName=item['startName'], endName=item['endName'],
+                            time=item['time'], roadGrade=item['roadGrade'], avgspeed=item['avgspeed'], sIndex=item['sIndex'],
+                            cIndex=item['cIndex'], bIndex=item['bIndex'], dir=item['dir'], rticLonlats=item['rticLonlats'],
+                            rticId=item['rticId'], vkt=item['vkt'])
+            self.cursor.execute(sql)
+            self.connect.commit()
+        except Exception as ex:
+            print(ex)
         return item
--- TrafficScrapy/TrafficScrapy/settings.py
+++ TrafficScrapy/TrafficScrapy/settings.py
@@ -36,7 +36,7 @@ MYSQL_TABLE_INFO = 'tra_info'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -61,9 +61,9 @@ MYSQL_TABLE_INFO = 'tra_info'
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'TrafficScrapy.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
